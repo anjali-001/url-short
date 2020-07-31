aa1aada08d404df6affe89800c03474b4fef4a79
Anjali <anjali001.as@gmail.com>
2020-07-31 23:40:33 +0530

    Page indentation again

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 function Landing() {
     return (
         <>
-          <div className="App-header">
+          <div className="App-header-landing">
           <h2 className="px-4" style={{backgroundColor:"#08353b",color:"#ffffff",borderRadius:"5px"}}> Bitter <span style={{backgroundColor:"white",padding:"0 3px"}}>links</span></h2>
 
             <Link to="/signup"><button className="btn btn-outline-secondary btn-style pr-5 pl-5 m-1">Sign Up</button></Link>
